8b0eede2716242c97cdf4fae6f2afae99ee21ea8
Tiago Jesus <tiago@lifebit.ai>
2022-05-24 19:24:55 +0100

    fix: SSE for multipart upload (#3)
    * fix: SSE for multipart upload
    
    * fix: removed SSE with Customer key parameter for multipart upload

--- filesystems/s3/src/main/java/org/lerch/s3fs/S3FileSystemProvider.java
+++ filesystems/s3/src/main/java/org/lerch/s3fs/S3FileSystemProvider.java
@@ -505,7 +505,6 @@ public class S3FileSystemProvider extends FileSystemProvider {
                             .copySourceRange("bytes=" + finalBytePosition + "-" + lastByte)
                             .bucket(target.getFileStore().name())
                             .key(target.getKey())
-                            .sseCustomerAlgorithm(AES256.name())
                             .partNumber(finalPartNum)
                             .build();
                     UploadPartCopyResponse uploadPartCopyResponse =  s3Client.uploadPartCopy(uploadPartCopyRequest);
