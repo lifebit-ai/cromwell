2ce11be368e200111a9414b046b0deafabab11a9
Tiago Jesus <tiago@lifebit.ai>
2022-10-17 11:26:46 +0100

    feat: added support for checking if optional outputs exist before copying to results (#5)
    * feat: added support for checking if optional outputs exist before copying to results
    
    * feat: added support for removing duplicates for both optional and required outputs

--- engine/src/main/scala/cromwell/engine/workflow/lifecycle/finalization/CopyWorkflowOutputsActor.scala
+++ engine/src/main/scala/cromwell/engine/workflow/lifecycle/finalization/CopyWorkflowOutputsActor.scala
@@ -13,7 +13,8 @@ import cromwell.core.path.{Path, PathCopier, PathFactory}
 import cromwell.engine.EngineWorkflowDescriptor
 import cromwell.engine.backend.{BackendConfiguration, CromwellBackends}
 import cromwell.filesystems.gcs.batch.GcsBatchCommandBuilder
-import wom.values.{WomSingleFile, WomValue}
+import wom.values.{WomSingleFile, WomValue, WomOptionalValue}
+import wom.graph.GraphNodePort.OutputPort
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
@@ -46,32 +47,60 @@ class CopyWorkflowOutputsActor(workflowId: WorkflowId, override val ioActor: Act
     }
   }
 
-  private def copyWorkflowOutputs(workflowOutputsFilePath: String): Future[Seq[Unit]] = {
-    val workflowOutputsPath = buildPath(workflowOutputsFilePath)
-    val outputFilePaths = getOutputFilePaths(workflowOutputsPath)
-
+  private def checkForDuplicatedFilePaths(outputFilePaths: List[(Path, Path)]) = {
     // Check if there are duplicated destination paths and throw an exception if that is the case.
     // This creates a map of destinations and source paths which point to them in cases where there are multiple
     // source paths that point to the same destination.
-    val duplicatedDestPaths: Map[Path, List[Path]] = outputFilePaths.groupBy{ case (_, destPath) => destPath}.collect {
-      case (destPath, list) if list.size > 1 => destPath -> list.map {case (source, _) => source}
+    val duplicatedDestPaths: Map[Path, List[Path]] = outputFilePaths.groupBy { case (_, destPath) => destPath }.collect {
+      case (destPath, list) if list.size > 1 => destPath -> list.map { case (source, _) => source }
     }
     if (duplicatedDestPaths.nonEmpty) {
       val formattedCollidingCopyOptions = duplicatedDestPaths.toList
-        .sortBy{case(dest, _) => dest.pathAsString} // Sort by destination path
+        .sortBy { case (dest, _) => dest.pathAsString } // Sort by destination path
         // Make a '/my/src -> /my/dest' copy tape string for each source and destination. Use flat map to get a single list
         // srcList is also sorted to get a deterministic output order. This is necessary for making sure the tests
         // for the error always succeed.
-        .flatMap{ case (dest, srcList) => srcList.sortBy(_.pathAsString).map(_.pathAsString + s" -> $dest")}
+        .flatMap { case (dest, srcList) => srcList.sortBy(_.pathAsString).map(_.pathAsString + s" -> $dest") }
       throw new IllegalStateException(
         "Cannot copy output files to given final_workflow_outputs_dir" +
-          s" as multiple files will be copied to the same path: \n${formattedCollidingCopyOptions.mkString("\n")}")}
+          s" as multiple files will be copied to the same path: \n${formattedCollidingCopyOptions.mkString("\n")}")
+    }
+  }
 
-    val copies = outputFilePaths map {
+  private def copyWorkflowOutputs(workflowOutputsFilePath: String): Future[Seq[Unit]] = {
+    val workflowOutputsPath = buildPath(workflowOutputsFilePath)
+
+    val optionalOutputFiles = getOptionalOutputs()
+    val optionalOutputFilePaths = getOutputFilePaths(workflowOutputsPath, optionalOutputFiles)
+
+    val requiredOutputFiles = getRequiredOutputs()
+    val requiredOutputFilePaths = getOutputFilePaths(workflowOutputsPath, requiredOutputFiles)
+
+    checkForDuplicatedFilePaths(requiredOutputFilePaths)
+    checkForDuplicatedFilePaths(optionalOutputFilePaths)
+
+    val requiredCopies = requiredOutputFilePaths map {
       case (srcPath, dstPath) => asyncIo.copyAsync(srcPath, dstPath)
     }
+
+    val optionalCopies = optionalOutputFilePaths map {
+      case (srcPath, dstPath) => {
+        asyncIo.existsAsync(srcPath).flatMap(
+          result => {
+            if (result) {
+              asyncIo.copyAsync(srcPath, dstPath)
+            } else {
+              // NOTE: When a file does not exist when uploading we return an empty future to avoid throwing while copying.
+              Future.successful(())
+            }
+          }
+        )
+      }
+    }
+
+    val allCopies = requiredCopies ::: optionalCopies
     
-    Future.sequence(copies)
+    Future.sequence(allCopies)
   }
 
   private def findFiles(values: Seq[WomValue]): Seq[WomSingleFile] = {
@@ -81,8 +110,16 @@ class CopyWorkflowOutputsActor(workflowId: WorkflowId, override val ioActor: Act
       }
     }
   }
+
+  private def getRequiredOutputs(): Map[OutputPort, WomValue] = {
+    workflowOutputs.outputs.filter((keyVal) => !keyVal._2.isInstanceOf[WomOptionalValue])
+  }
+
+  private def getOptionalOutputs(): Map[OutputPort, WomValue] = {
+    workflowOutputs.outputs.filter((keyVal) => keyVal._2.isInstanceOf[WomOptionalValue])
+  }
   
-  private def getOutputFilePaths(workflowOutputsPath: Path): List[(Path, Path)] = {
+  private def getOutputFilePaths(workflowOutputsPath: Path, workflowOutputsToFilter: Map[OutputPort, WomValue]): List[(Path, Path)] = {
 
     val useRelativeOutputPaths: Boolean = workflowDescriptor.getWorkflowOption(UseRelativeOutputPaths).contains("true")
     val rootAndFiles = for {
@@ -90,7 +127,7 @@ class CopyWorkflowOutputsActor(workflowId: WorkflowId, override val ioActor: Act
       backend <- workflowDescriptor.backendAssignments.values.toSeq
       config <- BackendConfiguration.backendConfigurationDescriptor(backend).toOption.toSeq
       rootPath <- getBackendRootPath(backend, config).toSeq
-      outputFiles = findFiles(workflowOutputs.outputs.values.toSeq).map(_.value)
+      outputFiles = findFiles(workflowOutputsToFilter.values.toSeq).map(_.value)
     } yield (rootPath, outputFiles)
 
     // This regex will make sure the path is relative to the execution folder.
